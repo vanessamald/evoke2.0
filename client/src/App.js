1a8bab34f4bc0194ebd34a40e257006107e0bb45
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2023-01-19 16:44:32 -0600

    connecting server to client and testing route

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,14 @@ import Home from './components/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [data, setData] = React.useState(null);
 
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+  
   return (
     <div>
       <main>
--- server/server.js
+++ server/server.js
@@ -3,6 +3,11 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const path = require('path');
 
+app.get("/api", (req, res) => {
+    res.json({ message: 'Testing Express' });
+  });
+  
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
+
