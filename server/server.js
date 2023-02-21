5d950d02c6a7eb0382b409dc685d0db88209dd41
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2023-02-20 22:13:52 -0600

    deploying to heroku

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.get("/api", (req, res) => {
   });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 app.post("/contact", (req, res) => {
